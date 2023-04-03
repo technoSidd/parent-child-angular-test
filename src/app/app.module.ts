0da2a53a1f5047cac3c9d92b8c61120d16dc820f
Ahmad Siddiqui <55476252+technoSidd@users.noreply.github.com>
2023-04-04 01:08:01 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-// import { MatRadioModule } from '@angular/material/radio';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ParentComponent } from "./parent/parent.component";
+import { ChildComponent } from "./child/child.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [AppComponent, ParentComponent, ChildComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
-    // MatRadioModule
-    // AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
